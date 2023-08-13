ea51846a6d8457762306a6f605749c43c6e6b748
Satyam Mishra <satyam.work.only@gmail.com>
2023-08-13 11:40:31 +0530

    added room msg chat

--- src/App.jsx
+++ src/App.jsx
@@ -1,205 +1,22 @@
-import {useState, useEffect, useRef} from 'react'
+import React from 'react';
 import './App.css'
-import {supabase} from "./supabaseClient";
-import {MyCard, OtherCard} from './components/card';
-import {useCookies} from 'react-cookie';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Home from './components/Home';
+import Chatroom from './components/Chatroom';
 
 
 function App() {
-    const [messages, setMessages] = useState([]);
-    const [message, setMessage] = useState({username: "", content: ""});
-    const {username, content} = message;
-    const [cookies, setCookie] = useCookies(['user']);
-    const messagesContainerRef = useRef(null);
-
-    useEffect(() => {
-        const storedUsername = localStorage.getItem("name") || cookies.name;
-        setMessage((prevMessage) => ({
-            ...prevMessage,
-            username: storedUsername
-        }));
-    }, []);
-
-    useEffect(() => {
-        const profiles = supabase.channel("*").on("postgres_changes", {
-            event: "INSERT",
-            schema: "public",
-            table: "profiles"
-        }, (payload) => {
-            console.log({payload});
-            payload && setMessages((oldMessages) => [
-                ...oldMessages,
-                payload.new
-            ]);
-            scrollToBottom();
-        }).subscribe();
-        return() => {
-            profiles.unsubscribe();
-        };
-    }, []);
-
-
-    useEffect(() => {
-        Init();
-    }, []);
-
-
-    async function Init() {
-        const {data: profiles} = await supabase.from("profiles").select("*");
-        if (profiles == null) 
-            return;
-        
-
-
-        setMessages(profiles);
-    }
-
-    // Insere USERNAME e CONTENT dentro da tabela
-    async function createPost() {
-        await supabase.from("profiles").insert([{
-                username,
-                content
-            }]).single();
-        setMessage({username: message.username, content: ""});
-        Init();
-        submit();
-        console.log(messages);
-    }
-
-    function submit() {
-        localStorage.setItem("name", message.username);
-        setCookie('name', message.username);
-    }
-
-
-    const scrollToBottom = () => {
-        if (messagesContainerRef.current) {
-            const container = messagesContainerRef.current;
-            container.scrollTop = container.scrollHeight;
-        }
-    };
-
-    useEffect(() => {
-        scrollToBottom(); // Scroll to bottom after messages update
-    }, [messages]);
-
-
     return (
-        <div> {/* component */}
-            <div className="bg-cover h-screen flex flex-col justify-center items-center" loading="lazy"
-                style={
-                    {backgroundImage: `url("https://source.unsplash.com/random?dark?aesthetic/1600x900")`}
-            }>
-                <h2 className='text-white md:text-3xl text-2xl'>Anon Exchange</h2>
-                {/* card  */}
-                <div className="w-11/12 lg:w-1/3 bg-white my-10 rounded-xl bg-opacity-60 backdrop-filter backdrop-blur-lg">
-                    {/* component  */}
-                    <div className="flex-1 sm:p-6 justify-between flex flex-col h-[464px] ">
-
-                        {/* top header section */}
-                        <div className="flex sm:items-center p-4 md:p-0 justify-between border-b-2 border-gray-100">
-                            <h1 className='pb-3'>Service status :
-                                <span>
-                                    🟢 Online</span>
-                            </h1>
-                        </div>
-
-                        {/* message section */}
-                        <div id="messages"
-                            ref={messagesContainerRef}
-                            // Set the ref for the messages container
-                            className="flex flex-col my-3 space-y-2 p-3 overflow-auto "
-                        >
-
-                            {
-                            messages ?. map((message) => ((message.username == cookies.name) ? <MyCard key={
-                                    message.id
-                                }
-                                username={
-                                    message.username
-                                }
-                                context={
-                                    message.content
-                                }/> : <OtherCard key={
-                                    message.id
-                                }
-                                username={
-                                    message.username
-                                }
-                                context={
-                                    message.content
-                                }/>))
-                        } </div>
-
-                        {/* bottom buttons and chat section */}
-                        <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-                            <div className='relative flex mb-2'>
-                                <input required
-                                    value={
-                                        (username)
-                                    }
-                                    onChange={
-                                        (e) => setMessage({
-                                            ...message,
-                                            username: e.target.value
-                                        })
-                                    }
-                                    type="text"
-                                    placeholder="Username"
-                                    className="w-1/2 focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-md py-3"/>
-                                <div className="absolute right-0 items-center inset-y-0 sm:flex">
-                                    <button type="button"
-                                        onClick={
-                                            () => {
-                                                submit();
-                                                scrollToBottom();
-                                                window.location.reload();
-                                            }
-                                        }
-                                        className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
-                                        <span className="font-bold">Set name</span>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="relative flex">
-                                {/* <span className="absolute inset-y-0 flex items-center">
-                                    <button type="button" className="inline-flex items-center justify-center rounded-full h-12 w-12 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
-                                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z"></path>
-                                        </svg>
-                                    </button>
-                                </span> */}
-                                <input required
-                                    value={content}
-                                    onChange={
-                                        (e) => {
-                                            setMessage({
-                                                ...message,
-                                                content: e.target.value
-                                            });
-                                            setCookie('name', e.target.value);
-                                        }
-                                    }
-                                    type="text"
-                                    placeholder="Write your message!"
-                                    className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-md py-3"/>
-                                <div className="absolute right-0 items-center inset-y-0 sm:flex">
-                                    <button onClick={createPost}
-                                        type="button"
-                                        className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
-                                        <span className="font-bold">Send</span>
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
-                                            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/"
+                    element={<Home/>}/>
+                <Route path="/chat_room/:roomCode"
+                    element={<Chatroom/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
 
 export default App
---
+++ src/components/dialogModel.jsx
@@ -0,0 +1,106 @@
+import React, {useState} from "react";
+import {supabase} from "../supabaseClient";
+import {Link, useNavigate} from "react-router-dom";
+
+
+const CreateRoomDialogModel = ({onClose}) => {
+
+    const [roomName, setRoomName] = useState('');
+    const [redirectToNewRoute, setRedirectToNewRoute] = useState(false);
+    const [roomCode, setRandomCode] = useState('');
+    let navigate = useNavigate();
+
+
+    const generateRandomCode = () => {
+        const code = Math.floor(100000 + Math.random() * 900000); // Generates a random 6-digit number
+        return code.toString();
+    };
+
+
+    const handleRoomNameChange = (event) => {
+        setRoomName(event.target.value);
+    };
+
+    const handleRoomNameSave = () => {
+        localStorage.setItem('room_name', roomName);
+        const code = generateRandomCode();
+        setRandomCode(code);
+
+        localStorage.setItem('room_code', code);
+        handleCreateRoom(code);
+
+        setRedirectToNewRoute(true);
+    };
+
+    const handleCreateRoom = async (code) => { // Save data to Supabase table
+        const {data, error} = await supabase.from('chat_room').upsert([{
+                room_name: roomName,
+                room_code: code,
+                message: `${roomName} room is created`
+            },]);
+
+        if (!error) {
+            alert(`Room ${roomName} created!`);
+        }
+    };
+
+
+    const handleJoinRoom = () => {
+        let path = `/chat_room/${roomCode}`;
+        navigate(path);
+
+        onClose();
+    }
+
+
+    return (
+        <div className="fixed left-0 top-0 z-[1055] w-full h-full overflow-y-auto overflow-x-hidden outline-none" tabIndex="-1" aria-labelledby="exampleModalCenterTitle" aria-modal="true" role="dialog">
+            <div className="pointer-events-none relative flex min-h-[calc(100%-1rem)] w-auto translate-y-[-50px] items-center transition-all duration-300 ease-in-out min-[576px]:mx-auto min-[576px]:mt-7 min-[576px]:min-h-[calc(100%-3.5rem)] min-[576px]:max-w-[500px]">
+                <div className="pointer-events-auto relative flex w-full flex-col rounded-2xl bg-opacity-60 backdrop-filter backdrop-blur-lg text-current shadow-lg border-[0.5px] ">
+                    <div className="flex flex-shrink-0 items-center justify-between rounded-t-md border-b-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                        <h5 className="text-xl font-medium leading-normal text-white dark:text-neutral-200" id="exampleModalScrollableLabel">
+                            Create a chat room
+                        </h5>
+                        <button onClick={onClose}
+                            type="button"
+                            className="box-content rounded-none border-none hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                            aria-label="Close">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="h-6 w-6 text-white">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                            </svg>
+                        </button>
+                    </div>
+
+                    <div className="relative py-4 px-2">
+                        <input className="px-2 py-2 w-full border-none outline-none text-white bg-transparent placeholder:text-gray-200 " type="text"
+                            value={roomName}
+                            onChange={handleRoomNameChange}
+                            placeholder="Enter room name"/>
+
+                        <div className="">
+                            {
+                            roomCode && <p className="text-white mt-4 mx-3 bg-slate-600 rounded-lg p-3">Room Code: {roomCode}</p>
+                        } </div>
+                    </div>
+
+                    <div className="flex flex-shrink-0 flex-wrap items-center justify-end rounded-b-md border-t-2 border-neutral-100 border-opacity-100 p-4 dark:border-opacity-50">
+                        <button onClick={handleRoomNameSave}
+                            type="button"
+                            className="ml-1 inline-block bg-primary bg-black rounded-lg text-white px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal">
+                            Create room
+                        </button>
+
+                        {
+                        roomCode && <button onClick={handleJoinRoom}
+                            type="button"
+                            className="ml-1 inline-block bg-primary bg-black rounded-lg text-white px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal">
+                            Join room
+                        </button>
+                    } </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateRoomDialogModel;
