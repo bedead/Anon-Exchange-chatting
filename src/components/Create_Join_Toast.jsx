2aa0fca27cb8501d55a3aaa68ca36e54aaa6c77e
Satyam Mishra <satyam.work.only@gmail.com>
2023-08-31 22:53:56 +0530

    added vercel config for routes

--- src/components/Create_Join_Toast.jsx
+++ src/components/Create_Join_Toast.jsx
@@ -2,7 +2,7 @@ const Create_Join_Toast = ({handlecreateDialogOpen, handlejoinDialogOpen}) => {
 
 
     return (
-        <div id="toast-message-cta" className="flex mx-auto md:absolute md:right-5 md:bottom-5 items-center max-w-xs p-2 md:p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
+        <div id="toast-message-cta" className="flex mx-auto md:absolute md:right-5 md:bottom-10 items-center max-w-xs p-2 md:p-4 space-x-4 text-gray-500 bg-white divide-x divide-gray-200 rounded-lg shadow-lg drop-shadow-lg dark:text-gray-400 dark:divide-gray-700 space-x dark:bg-gray-800" role="alert">
             <div className="flex">
 
                 <img className="w-8 h-8 rounded-full shadow-lg" src="https://avatars.dicebear.com/api/adventurer/indian.svg" alt="Jese Leos image"/>
@@ -10,12 +10,13 @@ const Create_Join_Toast = ({handlecreateDialogOpen, handlejoinDialogOpen}) => {
 
                     <span className="mb-1 text-sm font-semibold text-gray-900 dark:text-white">
                         Satyam Mishra</span>
-                    <div className="mb-2 text-sm font-normal">Hi, create chat rooms for quick discussions and private messaging.</div>
+                    <div className="mb-2 text-sm font-normal">Hi,this is Satyam Mishra creator of this application.
+                                                                    Now you can create chat rooms for quick discussions and private messaging.</div>
                     <div className="flex justify-evenly">
                         <button onClick={handlecreateDialogOpen}
-                            className="cursor-pointer inline-flex px-2.5 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Create Chat Room</button>
+                            className="cursor-pointer inline-flex px-2.5 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-3xl hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Create Chat Room</button>
                         <button onClick={handlejoinDialogOpen}
-                            className="cursor-pointer inline-flex px-2.5 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Join Room</button>
+                            className="cursor-pointer inline-flex px-2.5 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-3xl hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Join Room</button>
 
                     </div>
                 </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -148,7 +148,7 @@ const Home = () => {
 
                     {/* card  */}
 
-                    <div className="w-3/4 md:w-1/2 lg:w-1/3 bg-white my-10 rounded-xl bg-opacity-10 backdrop-filter backdrop-blur-lg">
+                    <div className="w-3/4 md:w-1/2 lg:w-1/3 bg-white my-10 rounded-xl shadow-lg bg-opacity-10 backdrop-filter backdrop-blur-lg">
                         {/* component  */}
                         <div className="flex-1 sm:p-6 justify-between flex flex-col h-[464px] ">
 
@@ -204,7 +204,7 @@ const Home = () => {
                                         }
                                         type="text"
                                         placeholder="Username"
-                                        className="w-1/2 focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-md py-3"/>
+                                        className="w-1/2 focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-3xl py-3"/>
                                     <div className="absolute right-0 items-center inset-y-0 sm:flex">
                                         <button type="button"
                                             onClick={
@@ -214,7 +214,7 @@ const Home = () => {
                                                     window.location.reload();
                                                 }
                                             }
-                                            className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+                                            className="inline-flex items-center justify-center rounded-3xl px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
                                             <span className="font-bold">Set name</span>
                                         </button>
                                     </div>
@@ -240,11 +240,11 @@ const Home = () => {
                                         }
                                         type="text"
                                         placeholder="Write your message!"
-                                        className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-md py-3"/>
+                                        className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-5 bg-gray-100 rounded-3xl py-3"/>
                                     <div className="absolute right-0 items-center inset-y-0 sm:flex">
                                         <button onClick={createPost}
                                             type="button"
-                                            className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+                                            className="inline-flex items-center justify-center rounded-3xl px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
                                             <span className="font-bold">Send</span>
                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
                                                 <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
